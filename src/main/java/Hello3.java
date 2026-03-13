91204bcc5d8bca9338c8c8009b249d383683f04c
hakgyogyojangchitsoldodook <designkim06@gmail.com>
2026-03-13 15:44:47 +0900

    소스 수정

---
+++ src/main/java/Hello3.java
@@ -0,0 +1,12 @@
+public class Hello3 {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello! My 3rd Java Practice!");
+
+
+    }
+
+
+
+}
---
+++ src/main/java/Hello5.java
@@ -0,0 +1,10 @@
+public class Hello5 {
+
+    public static void main(String[] args){
+
+        System.out.println("I Wanna Marry With You YuRim cause You are My whole Life supporter And Moonlight Crysh!!");
+
+    }
+
+
+}
