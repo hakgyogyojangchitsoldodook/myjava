d36afb93272571f5c9a056354e27fded65434bf9
hakgyogyojangchitsoldodook <designkim06@gmail.com>
2026-03-13 15:15:25 +0900

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,18 @@
+public class Hello {
+    public static void main(String[] args){
+      System.out.println("Hello!! My First Java Coding!!");
+      System.out.println("I wanna Be Some Fashion Star!!");
+      System.out.println("I'm Gonna Marry With My Gf In 28!!!!");
+      System.out.println("I Love Esdeekid So Bad");
+      System.out.println("I Love YuRim So Much This Is My Propose To You Babe.");
+
+
+
+    }
+
+
+
+
+
+
+}
