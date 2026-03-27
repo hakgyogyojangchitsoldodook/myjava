f48c9a5a3fcc7eb69f6407e14a534470fcf93529
hakgyogyojangchitsoldodook <designkim06@gmail.com>
2026-03-27 14:35:50 +0900

    소스 수정

--- src/main/java/Code03_12.java
+++ src/main/java/Code03_12.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
+
 public class Code03_12 {
     public static void main(String[] args){
-        int num = 99;
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        num = sc.nextInt();
 
         System.out.println( (num > 100) && (num < 200)) ;
         System.out.println( (num == 99) || (num == 100) );
         System.out.println( !(num == 100) );
 
+        sc.close();
 
     }
 }
