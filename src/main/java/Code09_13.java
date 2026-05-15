f5635e10986491c15a18eafebb3d139a2a787b83
hakgyogyojangchitsoldodook <designkim06@gmail.com>
2026-05-15 10:25:19 +0900

    소스 수정

--- src/main/java/Code09_13.java
+++ src/main/java/Code09_13.java
@@ -3,7 +3,7 @@ public class Code09_13 {
         int[][] ary = { {1, 2, 3, 4},
                     {5, 6, 7, 8},
                     {9, 10, 11, 12} };
-        System.out.printf("ary[0][0]부터 ary[2][3]까지 출력\n");
+        System.out.print("ary[0][0]부터 ary[2][3]까지 출력\n");
         for(int i = 0; i < 3; i++){
             for(int k =0; k < 4; k++){
                 System.out.printf("%3d",ary[i][k]);
