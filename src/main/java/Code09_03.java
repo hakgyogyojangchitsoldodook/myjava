0b5be4b7606d11c8a79ebb3aa4adc379ce2031c9
hakgyogyojangchitsoldodook <designkim06@gmail.com>
2026-05-15 09:47:47 +0900

    소스 수정

--- src/main/java/Code09_03.java
+++ src/main/java/Code09_03.java
@@ -11,7 +11,9 @@ public class Code09_03 {
             numAry[i] = s.nextInt();
         }
 
-        hap = numAry[0] + numAry[1] + numAry[2] + numAry[3];
+        for(int i = 0; i <= 3; i++){
+        hap += numAry[i];
+        }
         System.out.println("합계 ==>" + hap);
 
         s.close();
